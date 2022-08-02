e73a3c7e796c11dc09471ac66aa3f67f52c3b791
Bit Tensor <>
2022-08-02 13:40:22 -0400

    Add FTX logic

---
+++ src/ftx.ts
@@ -0,0 +1,149 @@
+import Decimal from "decimal.js"
+import fetch from "node-fetch"
+import { NewOrderReq, RestClient } from "ftx-api"
+
+export class FtxArbClient {
+	private client: RestClient
+	private market: FtxMarket
+	private account: FtxAccountInfo
+
+	constructor() {
+		this.client = new RestClient(
+			process.env.FTX_KEY,
+			process.env.FTX_SECRET,
+			{ subAccountName: process.env.FTX_SUBACCOUNT },
+		)
+	}
+
+	async init() {
+		this.market = await this.getMarket("SOL-PERP")
+	}
+
+	async getMarket(marketName: string): Promise<FtxMarket> {
+		const response = await fetch(
+			`https://ftx.com/api/markets/${marketName}`,
+		)
+		const result: any[] = (await response.json()).result
+		return this.toFtxMarket(result)
+	}
+
+	async refresh(): Promise<void> {
+		this.market = await this.getMarket("SOL-PERP")
+		return
+	}
+
+	getAsk(): Decimal {
+		return this.market.ask
+	}
+
+	getBid(): Decimal {
+		return this.market.bid
+	}
+
+	async getFtxAccountInfo(): Promise<FtxAccountInfo> {
+		const data = await this.client.getAccount()
+		const positionsMap: Record<string, FtxPosition> = {}
+		for (let i = 0; i < data.result.positions.length; i++) {
+			const positionEntity = data.result.positions[i]
+			const position = this.toFtxPosition(positionEntity)
+			positionsMap[position.future] = position
+		}
+
+		return {
+			freeCollateral: new Decimal(data.result.freeCollateral),
+			totalAccountValue: new Decimal(data.result.totalAccountValue),
+			// marginFraction is null if the account has no open positions
+			marginFraction: new Decimal(
+				data.result.marginFraction ? data.result.marginFraction : 0,
+			),
+			maintenanceMarginRequirement: new Decimal(
+				data.result.maintenanceMarginRequirement,
+			),
+			positionsMap: positionsMap,
+		}
+	}
+
+	async getPosition(ftxClient: any, marketId: string): Promise<FtxPosition> {
+		const data = await ftxClient.request({
+			method: "GET",
+			path: "/positions",
+		})
+		const positions: Record<string, FtxPosition> = {}
+		for (let i = 0; i < data.result.length; i++) {
+			const positionEntity = data.result[i]
+			if (positionEntity.future === marketId) {
+				const position = this.toFtxPosition(positionEntity)
+				positions[position.future] = position
+			}
+		}
+		return positions[marketId]
+	}
+
+	async getTotalPnLs(ftxClient: any): Promise<Record<string, number>> {
+		const data = await ftxClient.request({
+			method: "GET",
+			path: "/pnl/historical_changes",
+		})
+		return data.result.totalPnl
+	}
+
+	async placeOrder(payload: NewOrderReq): Promise<void> {
+		const data = await this.client.placeOrder(payload)
+	}
+
+	// noinspection JSMethodCanBeStatic
+	private toFtxMarket(market: any): FtxMarket {
+		return {
+			name: market.name,
+			bid: market.bid ? new Decimal(market.bid) : undefined,
+			ask: market.ask ? new Decimal(market.ask) : undefined,
+			last: market.last ? new Decimal(market.last) : undefined,
+		}
+	}
+
+	// noinspection JSMethodCanBeStatic
+	private toFtxPosition(positionEntity: any): FtxPosition {
+		return {
+			future: positionEntity.future,
+			netSize: new Decimal(positionEntity.netSize),
+			entryPrice: new Decimal(
+				positionEntity.entryPrice ? positionEntity.entryPrice : 0,
+			),
+			realizedPnl: new Decimal(
+				positionEntity.realizedPnl ? positionEntity.realizedPnl : 0,
+			),
+			cost: new Decimal(positionEntity.cost ? positionEntity.cost : 0),
+		}
+	}
+}
+
+export interface FtxAccountInfo {
+	freeCollateral: Decimal
+	totalAccountValue: Decimal
+	marginFraction: Decimal
+	maintenanceMarginRequirement: Decimal
+	positionsMap: Record<string, FtxPosition>
+}
+
+export interface PlaceOrderPayload {
+	market: string
+	side: string
+	price: null
+	size: number
+	type: string
+}
+
+export interface FtxPosition {
+	future: string
+	netSize: Decimal // + is long and - is short
+	entryPrice: Decimal
+	realizedPnl: Decimal
+	cost: Decimal
+}
+
+export interface FtxMarket {
+	name: string
+	bid: Decimal
+	ask: Decimal
+	last?: Decimal
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import {Commitment} from '@solana/web3.js'
+import { Commitment } from "@solana/web3.js"
 import {
-    Cluster,
-    getKeypairFromSecretKey,
-    sleep,
-    Wallet,
-    walletFromKeyPair,
-    Zamm,
-    ZAMM_SOL_KEY,
-    ZoUser
-} from '@zero_one/client'
-import {config} from 'dotenv'
-import Decimal from 'decimal.js'
+	Cluster,
+	getKeypairFromSecretKey,
+	sleep,
+	UpdateEvents,
+	Wallet,
+	walletFromKeyPair,
+	Zamm,
+	ZAMM_SOL_KEY,
+	ZoUser,
+} from "@zero_one/client"
+import { FtxArbClient } from "./ftx"
+import { config } from "dotenv"
+import Decimal from "decimal.js"
 
 /**
  * This loads the config data from .env.development.local or from .env.production.local
@@ -18,44 +20,57 @@ import Decimal from 'decimal.js'
  * Config examples are available in configExamples folder
  */
 
-
-config({path: `.env.${process.env.NODE_ENV || 'development'}.local`})
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` })
 const SECRET_KEY = process.env.SECRET_KEY
 const COMMITMENT: Commitment = process.env.COMMITMENT! as Commitment
-const SKIP_PREFLIGHT: boolean = process.env.SKIP_PREFLIGHT == 'true'
+const SKIP_PREFLIGHT: boolean = process.env.SKIP_PREFLIGHT == "true"
 const RPC_URL: string = process.env.RPC_URL!
 const CLUSTER: Cluster = process.env.CLUSTER! as Cluster
-
+const MIN_PROFIT_MARGIN: number = parseFloat(process.env.MIN_PROFIT_MARGIN!)
+const MAX_NOTIONAL_SIZE: number = parseFloat(process.env.MAX_NOTIONAL_SIZE!)
+const ORDER_SIZE: number = parseFloat(process.env.ORDER_SIZE!)
 
 async function run() {
-    // this is a simple way to load anchor wallet from Keypair
-    const keypair = getKeypairFromSecretKey(SECRET_KEY)
-    const wallet: Wallet = walletFromKeyPair(keypair)
+	// this is a simple way to load anchor wallet from Keypair
+	const keypair = getKeypairFromSecretKey(SECRET_KEY)
+	const wallet: Wallet = walletFromKeyPair(keypair)
+	const ftxClient: FtxArbClient = new FtxArbClient()
+	await ftxClient.init()
 
-    const arbUser = await ZoUser.load(wallet, CLUSTER, {
-        withRealm: false,
-        commitment: COMMITMENT,
-        skipPreflight: SKIP_PREFLIGHT,
-        rpcUrl: RPC_URL
-    })
-    const zamm = await Zamm.load(arbUser.program, ZAMM_SOL_KEY, arbUser.state, COMMITMENT)
+	const arbUser = await ZoUser.load(wallet, CLUSTER, {
+		withRealm: false,
+		commitment: COMMITMENT,
+		skipPreflight: SKIP_PREFLIGHT,
+		rpcUrl: RPC_URL,
+	})
+	const zamm = await Zamm.load(
+		arbUser.program,
+		ZAMM_SOL_KEY,
+		arbUser.state,
+		COMMITMENT,
+	)
 
-    /**
-     * Here arbUser and zamm subscribe to changes and monitor them across Zero One exchange including arber margin, oracle prices, zamm
-     * xSensitivity=10 means that event will be only fired if X changes by at least 1/10=0.1 SOL-PERP in balance
-     * ySensitivity=100 means that event will be only fired if Y changes by at least 1/100=0.01 USD in balance
-     */
-    await arbUser.subscribe(false)
-    const xSensitivity = 10, ySensitivity = 100
-    await zamm.subscribe(xSensitivity, ySensitivity)
+	/**
+	 * Here arbUser and zamm subscribe to changes and monitor them across Zero One exchange including arber margin, oracle prices, zamm
+	 * xSensitivity=10 means that event will be only fired if X changes by at least 1/10=0.1 SOL-PERP in balance
+	 * ySensitivity=100 means that event will be only fired if Y changes by at least 1/100=0.01 USD in balance
+	 */
+	await arbUser.subscribe(false)
+	const xSensitivity = 10,
+		ySensitivity = 100
+	await zamm.subscribe(xSensitivity, ySensitivity)
 
-    async function arb({
-                           X,
-                           Y,
-                           price
-                       }: { X: Decimal, Y: Decimal, price: Decimal }) {
-        console.log(X.toNumber(), Y.toNumber(), price.toNumber())
-        /*
+	async function arb({
+		X,
+		Y,
+		price,
+	}: {
+		X: Decimal
+		Y: Decimal
+		price: Decimal
+	}) {
+		console.log(X.toNumber(), Y.toNumber(), price.toNumber())
+		/*
         X - corresponds to the amount of SOL/SOL-PERP in the ZAMM (Decimal)
         Y - corresponds to the amount of USD in the ZAMM (Decimal)
         price - corresponds to the price of SOL-PERP according to ZAMM
@@ -79,46 +94,114 @@ async function run() {
 
          //at the same time one can add below the code to call other DEX/CEX to hedge the exposure from 01.
          */
-    }
+		await ftxClient.refresh()
+		const ftx_price = ftxClient.getAsk().add(ftxClient.getBid()).div(2)
 
-    // Here is a simple event listener which responds to any changes affecting X or Y in ZAMM
-    // zamm.eventEmitter!.addListener(UpdateEvents.zammModified, arb)
+		const diff = ftx_price.sub(price).div(price.add(ftx_price).div(2))
 
+		console.log(
+			`Zamm price is $${price} and FTX price is $${ftx_price}: diff is ${diff.mul(
+				100,
+			)}%`,
+		)
 
-    /**
-     * this is an alternative to arb function which allows locked access to the arb function, so one cannot avoid worrying about arbing twice at the same time before finishing the first arb
-     * WARNING: this is not the safest implementation. Recommend implementing a separate solution depending on one's needs.
-     */
-    const arbUpdatesQueue: Array<{ X: Decimal, Y: Decimal, price: Decimal }> = []
-    let locked = false
+		const open_notional = arbUser.totalPositionNotional
+		const isLong = arbUser.positions.find((pos) => {
+			if (pos.marketKey === "SOL-PERP") {
+				return pos.isLong
+			}
+		})
 
-    function lockedArb({
-                           X,
-                           Y,
-                           price
-                       }: { X: Decimal, Y: Decimal, price: Decimal }) {
-        if (locked) {
-            arbUpdatesQueue.push({
-                X,
-                Y,
-                price
-            })
-            return
-        }
+		const size =
+			Math.min(ORDER_SIZE, MAX_NOTIONAL_SIZE - open_notional.toNumber()) /
+			ftx_price.toNumber()
 
-        locked = true
+		const can_open_short = size > 0 || isLong
+		const can_open_long = size > 0 || !isLong
 
-        arb({X, Y, price})
+		if (diff.lessThan(-MIN_PROFIT_MARGIN / 100) && can_open_short) {
+			console.log(`Shorting Zamm for ${size} SOL-PERP at $${price}.`)
+			console.log(`Longing FTX for ${size} SOL-PERP at $${ftx_price}.`)
+			/*
+			await zamm.limitArb(
+				arbUser.margin,
+				size,
+				price.mul(0.999).toNumber(),
+				false,
+			)
+			await ftxClient.placeOrder({
+				market: "SOL-PERP",
+				price: ftx_price.mul(1.001).toNumber(),
+				side: "buy",
+				type: "limit",
+				size: size,
+			})
+			*/
+		} else if (diff.greaterThan(MIN_PROFIT_MARGIN / 100) && can_open_long) {
+			console.log(`Longing Zamm for ${size} SOL-PERP at $${price}.`)
+			console.log(`Shorting FTX for ${size} SOL-PERP at $${ftx_price}.`)
+			/*
+			await zamm.limitArb(
+				arbUser.margin,
+				size,
+				price.mul(1.00001).toNumber(),
+				true,
+			)
+			await ftxClient.placeOrder({
+				market: "SOL-PERP",
+				price: ftx_price.mul(0.999).toNumber(),
+				side: "sell",
+				type: "limit",
+				size: size,
+			})
+			*/
+		} else {
+			console.log(`No arb`)
+		}
+	}
 
-        if (arbUpdatesQueue.length > 0) {
-            locked = false
-            const update = arbUpdatesQueue.shift()!
-            lockedArb(update)
-        }
-    }
+	// Here is a simple event listener which responds to any changes affecting X or Y in ZAMM
+	//zamm.eventEmitter!.addListener(UpdateEvents.zammModified, arb)
 
-    // Here is a simple event listener which responds to any changes affecting X or Y in ZAMM
-    // zamm.eventEmitter!.addListener(UpdateEvents.zammModified, lockedArb)
+	/**
+	 * this is an alternative to arb function which allows locked access to the arb function, so one cannot avoid worrying about arbing twice at the same time before finishing the first arb
+	 * WARNING: this is not the safest implementation. Recommend implementing a separate solution depending on one's needs.
+	 */
+	const arbUpdatesQueue: Array<{ X: Decimal; Y: Decimal; price: Decimal }> =
+		[]
+	let locked = false
+
+	function lockedArb({
+		X,
+		Y,
+		price,
+	}: {
+		X: Decimal
+		Y: Decimal
+		price: Decimal
+	}) {
+		if (locked) {
+			arbUpdatesQueue.push({
+				X,
+				Y,
+				price,
+			})
+			return
+		}
+
+		locked = true
+
+		arb({ X, Y, price })
+
+		if (arbUpdatesQueue.length > 0) {
+			locked = false
+			const update = arbUpdatesQueue.shift()!
+			lockedArb(update)
+		}
+	}
+
+	// Here is a simple event listener which responds to any changes affecting X or Y in ZAMM
+	zamm.eventEmitter!.addListener(UpdateEvents.zammModified, lockedArb)
 }
 
 run().then()
